806f0d5c66ad4b8c34470ac687bcee0d9357dea1
Povilas Balciunas <balciunas90@gmail.com>
2016-09-28 14:11:22 +0300

    Use separate object to hold benchmark results

--- pyhttp/summary.py
+++ pyhttp/summary.py
@@ -1,6 +1,6 @@
 """Benchmark summary related utilities."""
 
-from typing import List
+from typing import List, Dict
 
 
 def warn_sigint() -> None:
@@ -23,7 +23,25 @@ def warn_sigint() -> None:
     print('\x1b[2;47;37m')
 
 
-def results_to_str(stats: list, times: List[float], concurrency: int) -> None:
+class BenchmarkResults:
+    def __init__(self, min_doc_len: int, avg_doc_len: int, max_doc_len: int,
+                 concurrency: int, completed_requests: int, reqs_per_sec: int,
+                 min_conn_time: int, avg_conn_time: int, max_conn_time: int,
+                 status_codes: Dict[int, int]) -> None:
+        self.min_doc_len = min_doc_len
+        self.avg_doc_len = avg_doc_len
+        self.max_doc_len = max_doc_len
+        self.concurrency = concurrency
+        self.completed_requests = completed_requests
+        self.reqs_per_sec = reqs_per_sec
+        self.min_conn_time = min_conn_time
+        self.avg_conn_time = avg_conn_time
+        self.max_conn_time = max_conn_time
+        self.status_codes = status_codes
+
+
+def make_benchmark_results(stats: list, times: List[float],
+                 concurrency: int) -> BenchmarkResults:
     retcode = {}
     retcode[200] = 0
     retcode[404] = 0
@@ -44,16 +62,27 @@ def results_to_str(stats: list, times: List[float], concurrency: int) -> None:
         if 'status' in stat:
             inc(retcode, stat['status'])
 
+    return BenchmarkResults(
+            min(size), avg(size), max(size), concurrency, completed_results,
+            completed_results / (times[4] - times[3]),
+            min(time), avg(time), max(time), retcode)
+
+
+def results_to_str(stats: list, times: List[float], concurrency: int) -> None:
+    results = make_benchmark_results(stats, times, concurrency)
     lines = [
-        'Document Length:    [min: %d, avg: %f, max: %d] Bytes' % (min(size), avg(size), max(size)),
-        'Concurrency Level:    %d' % (concurrency),
-        'Complete requests:    %d' % (completed_results),
-        'Requests per second:    %f [#/sec] (mean)' % (completed_results / (times[4] - times[3])),
-        'Connection Times Total:    [min: %f, avg: %f, max: %f] seconds' % (min(time), avg(time), max(time)),
+        'Document Length:    [min: %d, avg: %f, max: %d] Bytes' \
+            % (results.min_doc_len, results.avg_doc_len, results.max_doc_len),
+        'Concurrency Level:    %d' % (results.concurrency),
+        'Complete requests:    %d' % (results.completed_requests),
+        'Requests per second:    %f [#/sec] (mean)' % (results.reqs_per_sec),
+        'Connection Times Total:    [min: %f, avg: %f, max: %f] seconds' \
+            % (results.min_conn_time, results.avg_conn_time,
+               results.max_conn_time),
         'Status codes:',
     ]
-    for code in retcode:
-        lines.append('\t%s %d' % (code, retcode[code]))
+    for status_code, count in results.status_codes.items():
+        lines.append('\t%s %d' % (status_code, count))
 
     return '\n'.join(lines)
 
